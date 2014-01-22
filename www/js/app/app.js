7fe633d512f251572dd4fd4fc649bf8cbc98a957
Sergio Gimeno <gimeno.sergio@gmail.com>
2014-01-22 21:04:15 +0100

    Use Cordova File API to get camera pictures. Tested only in Android 4.2

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,23 +1,22 @@
 angular.module('ngMemory', [])
 
-.controller('MainCtrl', function($scope, cordovaService){
+.controller('MainCtrl', function($scope, cordovaService, picturesService){
 
 	$scope.greeting = 'Apache Cordova';
 
-	$scope.received = false;
+	picturesService.readPictures()
+		.then(function(pictures){
+			var len = pictures.length;
+			if (len < 6)
+				alert('Lenght is ' + len + '. Go take some pictures :)');
 
-	cordovaService.$ready
-		.then(function(device){
-			$scope.received = true;
-			$scope.greeting = 'You are on ' + device.platform + ' ' + device.version;
-			console.log('deviceready');
+			$scope.entries = pictures;
 		});
-
 })
 
 .service('cordovaService', function($window, $q) {
 
-    // Source https://github.com/mgcrea/angular-cordova/blob/master/src/angular-cordova.js
+    // Source https://github.com/mgcrea/angular-cordova
 
     var deferredReady = $q.defer();
     this.$ready = deferredReady.promise;
@@ -25,6 +24,72 @@ angular.module('ngMemory', [])
     document.addEventListener('deviceready', function() {
       deferredReady.resolve($window.device);
     });
+
+})
+
+.factory('picturesService', function($window, $q, cordovaService) {
+
+
+	function readPictures(fileSystemRoot){
+		var deferred = $q.defer();
+
+		fileSystemRoot.getDirectory('DCIM/Camera', {create:false}, function(dirEntry){
+			
+			// Get a directory reader
+			var directoryReader = dirEntry.createReader();
+
+			// Get a list of all the entries in the directory
+			directoryReader.readEntries(success,fail);
+		});
+
+		function success(entries) {
+		    deferred.resolve(entries);
+		}
+
+		function fail(error) {
+		    deferred.reject("Failed to list directory contents");
+		    alert("Failed to list directory contents: " + error.code);
+		}
+		
+
+		return deferred.promise;
+	}
+
+	function getFileSystem() {
+	    var deferred = $q.defer();
+
+	    function fail() {
+	        var msg = "failed to get filesystem";
+	        deferred.reject(msg)
+	        console.log(msg);
+	    }
+	    function gotFS(fileSystem) {
+	        deferred.resolve(fileSystem.root);
+	        console.log("got filesystem: " + fileSystem.root.fullPath);
+	    }
+        
+        window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+        return deferred.promise;
+    }
+
+
+
+	return {
+		getFileSystem: function(){
+			return cordovaService.$ready
+				.then(getFileSystem);
+		},
+		readPictures: function(){
+
+			return cordovaService.$ready
+				.then(getFileSystem)
+				.then(readPictures);
+	
+		}
+	}
 
 })
 
